bf5079ba2700c1d627a7fc1e62b54e386c5850eb
Luan CORTES ZUKA <LuanCort@edueptm.ch>
2021-06-27 19:46:26 +0200

    Ajoutez des fichiers projet.

---
+++ CurrencyConvert.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace CurrencyConverter
+{
+    class CurrencyConvert
+    {
+
+        private List<List<string>> sListCurrency;
+        private double dMontantMonnaie1;
+        private double dMontantMonnaie2;
+        private string sMonnaie1;
+        private string sMonnaie2;
+
+
+
+        public CurrencyConvert(double MontantMonnaie1, double MontantMonnaie2, string Monnaie1, string Monnaie2)
+        {
+            dMontantMonnaie1 = MontantMonnaie1;
+            dMontantMonnaie2 = MontantMonnaie2;
+            sMonnaie1 = Monnaie1;
+            sMonnaie2 = Monnaie2;
+        }
+
+
+        public void RemplirCombobox(ComboBox combobox)
+        {
+            InstancierListe();
+        }
+
+        private void InstancierListe()
+        {
+            sListCurrency = new List<List<string>>(23);
+
+            for (int L = 0; L < 8; L++)
+            {
+                for (int C = 0; C < 8; C++)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double dMontantMonnaie1;
+        double dMontantMonnaie2;
+        string sMonnaie1;
+        string sMonnaie2;
+        CurrencyConvert Currency;
+        public MainWindow()
+        {
+            dMontantMonnaie1 = double.Parse(txtMonnaie1.Text);
+            dMontantMonnaie2 = double.Parse(txtMonnaie2.Text);
+            sMonnaie1 = cmboxMonnaie1.Text;
+            sMonnaie2 = cmboxMonnaie2.Text;
+
+            Currency = new CurrencyConvert(dMontantMonnaie1, dMontantMonnaie2, sMonnaie1, sMonnaie2);
+
+            InitializeComponent();
+            Currency.RemplirCombobox(cmboxMonnaie1);
+            Currency.RemplirCombobox(cmboxMonnaie2);
+
+
+
+        }
+    }
+}
